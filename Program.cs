eed62daf92e8031692760ee708c49d6a04fefb49
Radhika <radhikasn06@gmail.com>
2024-02-16 10:49:47 +0530

    success

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ Console.WriteLine( "Add Call");
 calc.Add(12, 45);
 
 Console.WriteLine("Multi Call");
-calc.Add(12.2, 45.3);
+calc.Multi(12.2, 45.3);
 
+Console.WriteLine("Sub Call");
+calc.Sub(12.2, 45.3);
+Console.WriteLine("div Call");
+calc.div(12.2, 45.3);
 
